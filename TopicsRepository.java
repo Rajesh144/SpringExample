6e28977957a776c097b7e7c01fc492e9d99a7f30
Rajesh144 <rajbandi6@gmail.com>
2018-07-15 23:41:27 -0500

    Add files via upload

---
+++ TopicsRepository.java
@@ -0,0 +1,8 @@
+package com.rajesh.springbootstarter.topics;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicsRepository extends CrudRepository<Topics,String> {
+	
+	
+}
